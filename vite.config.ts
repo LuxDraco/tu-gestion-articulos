bcf14487aad5d00e2742b5637faefb0fdab1b4c6
Jose Alfonso Gomez Coeto <ponchogcja@gmail.com>
2025-01-31 15:13:50 -0600

    Add testing, Tailwind setup, and update configurations
    Added Vitest scripts for testing, Tailwind CSS integration, and enhanced tsconfig settings including path aliases. Adjusted Vite config with Tailwind and testing setup. Updated project metadata and ignored `package-lock.json`.

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,27 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {fileURLToPath} from 'url';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-            '@core': path.resolve(__dirname, './src/core'),
-            '@features': path.resolve(__dirname, './src/features'),
-            '@shared': path.resolve(__dirname, './src/shared'),
-            '@store': path.resolve(__dirname, './src/store')
-        }
+        alias: [
+            { find: '@', replacement: path.resolve(__dirname, 'src') },
+            { find: '@core', replacement: path.resolve(__dirname, 'src/core') },
+            { find: '@features', replacement: path.resolve(__dirname, 'src/features') },
+            { find: '@shared', replacement: path.resolve(__dirname, 'src/shared') },
+            { find: '@store', replacement: path.resolve(__dirname, 'src/store') }
+        ]
+    },
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/test/setup.ts',
+        css: true
     }
 });
